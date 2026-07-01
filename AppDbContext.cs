06e4267b89d4b572558d91a7ae907982af5f298d
Demarut <ilya.demyanov.2005@gmail.com>
2026-07-01 14:16:53 +0300

    Delete AppDbContext.cs

--- AppDbContext.cs
+++
@@ -1,8 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-public class AppDbContext : DbContext {
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
-    public DbSet<CarModel> CarModels => Set<CarModel>();
-    public DbSet<ServiceStation> ServiceStations => Set<ServiceStation>();
-    public DbSet<RepairRequest> RepairRequests => Set<RepairRequest>();
-}
