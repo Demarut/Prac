b46913a8b7d89ef5652a1997136bfde9d55a7619
Demarut <ilya.demyanov.2005@gmail.com>
2026-07-04 14:12:09 +0300

    Models.cs

--- ServiceCenterApp/Models.cs
+++ ServiceCenterApp/Models.cs
@@ -1,18 +1,24 @@
 public class CarModel {
     public int Id { get; set; }
-    public string ModelName { get; set; } = "";
+    [Required]
+    public string ModelName { get; set; } = null!;
 }
 
 public class ServiceStation {
     public int Id { get; set; }
-    public string Name { get; set; } = "";
-    public string Address { get; set; } = "";
+    [Required]
+    public string Name { get; set; } = null!;
+    [Required]
+    public string Address { get; set; } = null!;
 }
 
 public class RepairRequest {
     public int Id { get; set; }
-    public int CarModelId { get; set; }
-    public int ServiceStationId { get; set; }
-    public string IssueDescription { get; set; } = "";
-    public DateTime CreatedAt { get; set; } = DateTime.Now;
+    [Required]
+    public int CarModelId { get; set; } = null!;
+    [Required]
+    public int ServiceStationId { get; set; } = null!;
+    [Required]
+    public string IssueDescription { get; set; } = null!;
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 }
